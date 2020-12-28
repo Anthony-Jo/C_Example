3488158bc659f7e56723be4e8b79cd6767f4151d
AnthonyJo <joseongkwang@gmail.com>
2020-12-29 00:10:59 +0900

    commit push with vs code

--- hello_world.c
+++ hello_world.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
+#define FALSE   0
+#define TRUE    1
+
 void main(void) {
     printf("Github commit & push\n");
     printf("visual studio code 컴파일\n");
+
+    int flag = FALSE;
+
+    if (flag) {
+        printf("flag is false\n");
+    } else {
+        printf("flag is true\n");
+    }
 }
